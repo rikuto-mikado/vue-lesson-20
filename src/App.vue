29ac753345463c3711636e99022e422312e0b441
Rikuto <mikadorikuto@gmail.com>
2026-03-18 16:44:27 +0900

    code updated

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <learning-resource></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+    components: {
+        // You can also use shorthand: LearningResource
+        LearningResource : LearningResource
+    },
+    data() {
+        return {
+            storedResources: [
+                { 
+                    id: 'official-guide', 
+                    title: 'Official Guide', 
+                    description: 'The Official Vue documentation.',
+                    link: 'https://vuejs.org'
+                },
+                { 
+                    id: 'google', 
+                    title: 'Google', 
+                    description: 'Learn to google...',
+                    link: 'https://google.org'
+                }
+            ]
+        };
+    },
+
+}
+</script>
