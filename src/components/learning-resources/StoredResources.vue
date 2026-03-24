5f36ad9dc95f137b5a358b294ecd67366f21fd96
Rikuto <mikadorikuto@gmail.com>
2026-03-24 22:43:50 +0900

    code updated

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
